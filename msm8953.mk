e554d4941ac8bca1a8a31a4d427738ee0466b900
Kyuofox <88403766@qq.com>
2022-11-22 18:07:47 +0530

    msm8953-common: build libavservices_minijail and vendor variant too
    CANNOT LINK EXECUTABLE "/system/vendor/bin/ims_rtp_daemon": library "libavservices_minijail.so" not found: needed by main executable
    
    Signed-off-by: DhruvChhura <dhruvchhura18@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -242,6 +242,10 @@ PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
